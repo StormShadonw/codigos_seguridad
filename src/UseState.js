995ebae8f0e720f51b7c3dc2a5f9d638d57ac19c
luis lopez <llopez@elcatador.com>
2024-12-18 17:36:58 -0400

    UseEffect

--- src/UseState.js
+++ src/UseState.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 function UseState({name}) {
     const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }
+        , 2000);
+    }, [loading]);
 
     return (
         <div>
         <h2>Eliminar {name}</h2>
         <p>Escribe el codigo de seguridad</p>
         {error && <p>Error: codigo incorrecto</p>}
+        {loading && <p>Cargando...</p>}
         <input type="text" placeholder='Codigo de seguridad' />
         <button onClick={() => setError(!error)}>Comprobar</button>
     </div>
