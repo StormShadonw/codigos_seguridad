9dd2ad92377a9ddac07a70c4c2f87375f5cc16aa
luis lopez <llopez@elcatador.com>
2025-01-08 18:06:32 -0400

    estados imperativos con useState

--- src/UseState.js
+++ src/UseState.js
@@ -7,6 +7,8 @@ const [state, setState] = useState({
     codigoValue: "",
     error: false,
     loading: false,
+    confirmed: false,
+    deleted: false,
 });
 
     // const [codigoValue, setCodigoValue] = React.useState("");
@@ -28,46 +30,91 @@ const [state, setState] = useState({
 
     // }, [loading]);
 
-    return (
-        <div>
-        <h2>Eliminar {name}</h2>
-        <p>Escribe el codigo de seguridad</p>
-        {state.error && <p>Error: codigo incorrecto</p>}
-        {state.loading && <p>Cargando...</p>}
-        <input type="text" placeholder='Codigo de seguridad'
-        value={state.codigoValue}
-        onChange={(e) => {
-            setState({
-                ...state,
-                codigoValue: e.target.value, error: false,});
-        }}
-        />
-        <button onClick={() => {
-            setState({
-                ...state,
-                loading: true,
-                error: false,
-            });
-            setTimeout(() => {
-                if(state.codigoValue !== SECURITY_CODE) {
-                    console.log("Codigo incorrecto");
-                    setState({
-                        ...state,
-                        error: true,
-                        loading: false,
-                    });
-                } else {
-                    setState({
-                        ...state,
-                        loading: false,
-                    });
+    if(!state.deleted && !state.confirmed) {
+        return (
+            <div>
+            <h2>Eliminar {name}</h2>
+            <p>Escribe el codigo de seguridad</p>
+            {state.error && <p>Error: codigo incorrecto</p>}
+            {state.loading && <p>Cargando...</p>}
+            <input type="text" placeholder='Codigo de seguridad'
+            value={state.codigoValue}
+            onChange={(e) => {
+                setState({
+                    ...state,
+                    codigoValue: e.target.value, error: false,});
+            }}
+            />
+            <button onClick={() => {
+                setState({
+                    ...state,
+                    loading: true,
+                    error: false,
+                });
+                setTimeout(() => {
+                    if(state.codigoValue !== SECURITY_CODE) {
+                        console.log("Codigo incorrecto");
+                        setState({
+                            ...state,
+                            error: true,
+                            loading: false,
+                        });
+                    } else {
+                        setState({
+                            ...state,
+                            loading: false,
+                            confirmed: true,
+                        });
+                    }
+    
                 }
-
+                , 2000);
+            }}>Comprobar</button>
+        </div>
+        );
+    } else if(state.confirmed && !state.deleted) {
+    return (<React.Fragment>
+            <h2>Eliminar {name}</h2>
+            <p>Esta seguro de que quiere eliminar este codigo?</p>
+            <button
+            onClick={() => {
+                setState({
+                    ...state,
+                    deleted: true,
+                });
             }
-            , 2000);
-        }}>Comprobar</button>
-    </div>
-    );
+        }
+            >Si, eliminar</button>
+            <button
+            onClick={() => {
+                setState({
+                    ...state,
+                    confirmed: false,
+                });
+            }}
+            >No</button>
+    </React.Fragment>)
+    } else {
+return (<React.Fragment>
+                <h2>Eliminar {name}</h2>
+
+            <button
+            onClick={() => {
+                setState({
+                    ...state,
+    codigoValue: "",
+    error: false,
+    loading: false,
+    confirmed: false,
+    deleted: false,
+                });
+            }
+        }
+            >Recuperar UseState</button>
+            
+</React.Fragment>)
+    }
+
 }
 
 export { UseState };
